c29a9e9bfe5a942fd71dc26e78d140d400502ae0
SonDH <hs5011@gmail.com>
2026-02-11 09:31:15 +0700

    cập nhật tìm kiếm bản đồ

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: './', // Đảm bảo chạy đúng trên GitHub Pages dù là thư mục con
+      build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        emptyOutDir: true,
+      },
       server: {
         port: 3000,
         host: '0.0.0.0',
